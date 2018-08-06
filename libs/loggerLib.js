30e044b8d57788c5b8276b8191fd6ec6731b1e86
Gajal Vasita <gajalchouhan2404@gmail.com>
2018-08-06 21:47:16 +0530

    Update loggerLib.js

--- libs/loggerLib.js
+++ libs/loggerLib.js
@@ -1,35 +1,33 @@
 const logger = require('pino')()
+
 const moment = require('moment')
 
-let captureError = (errorMessage, errorOrigin, errorLevel) => {
-  let currentTime = moment()
-
-  let errorResponse = {
-    timestamp: currentTime,
-    errorMessage: errorMessage,
-    errorOrigin: errorOrigin,
-    errorLevel: errorLevel
-  }
-
-  logger.error(errorResponse)
-  return errorResponse
-} // end captureError
+let captureerror = (errorMessage, errorOrigin, errorLevel) => {
+    let currentTime = moment();
+    let errorResponse = {
+        timeStamp: currentTime,
+        errorMessage: errorMessage,
+        errorOrigin: errorOrigin,
+        errorLevel: errorLevel
+    }
+    logger.error(errorResponse)
+    return errorResponse
+}
 
 let captureInfo = (message, origin, importance) => {
-  let currentTime = moment()
+    let currentTime = moment()
+    let infoMessage = {
+        timeStamp: currentTime,
+        message: message,
+        origin: origin,
+        level: importance
+    }
+    logger.info(infoMessage)
+    return infoMessage
+}
 
-  let infoMessage = {
-    timestamp: currentTime,
-    message: message,
-    origin: origin,
-    level: importance
-  }
-
-  logger.info(infoMessage)
-  return infoMessage
-} // end infoCapture
 
 module.exports = {
-  error: captureError,
-  info: captureInfo
+    captureerror: captureerror,
+    captureInfo: captureInfo
 }
